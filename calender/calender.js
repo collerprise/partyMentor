5fae38dbe1571ebf8a28bcdc55b3ce65a4bb5d77
Nicolas <collerprise@gmail.com>
2015-03-23 13:41:20 +0100

    2015-03-23 making all branches

--- calender/calender.js
+++ calender/calender.js
@@ -2,6 +2,7 @@ function main () {
 	var calenderPane = document.getElementById("calender-pane");
 	calenderPane.innerHTML = "calender-pane";
 	calenderPane.style.backgroundColor = "#CCDDCC";
+	// more stuff goes here
 }
 
 main();
