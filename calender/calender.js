63416b3af8544e01841a385080d3bd5cf822268c
Nicolas <collerprise@gmail.com>
2015-03-23 13:46:44 +0100

    illias branch maken

--- calender/calender.js
+++ calender/calender.js
@@ -2,6 +2,7 @@ function main () {
 	var calenderPane = document.getElementById("calender-pane");
 	calenderPane.innerHTML = "calender-pane";
 	calenderPane.style.backgroundColor = "#CCDDCC";
+	// commit for illias branch
 }
 
 main();
