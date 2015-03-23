9452224d79778ddfb7405d7daf3013324551b513
Nicolas <collerprise@gmail.com>
2015-03-23 13:29:10 +0100

    2015-03-23 nicolas making dev-nicolas branch

--- calender/calender.js
+++ calender/calender.js
@@ -2,7 +2,6 @@ function main () {
 	var calenderPane = document.getElementById("calender-pane");
 	calenderPane.innerHTML = "calender-pane";
 	calenderPane.style.backgroundColor = "#CCDDCC";
-	//for (var i = 0, )
 }
 
 main();
