c3474eb7ebd8dfda7102c1c10b70389a5421508d
Nicolas <collerprise@gmail.com>
2015-03-23 10:51:33 +0100

    2015-03-23 nicolas adding jslintrc file

--- calender/calender.js
+++ calender/calender.js
@@ -1,1 +1,7 @@
+function main () {
+	var calenderPane = document.getElementById("calender-pane");
+	calenderPane.innerHTML = "calender-pane";
+	calenderPane.style.backgroundColor = "#CCDDCC";
+}
 
+main();
